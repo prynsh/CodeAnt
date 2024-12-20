95a0e346da3592613cc2f434cb4e95be0f5eb087
Priyansh Verma <priyansh2112@gmail.com>
2024-12-20 15:51:10 +0530

    Landing Page Changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+'use client';
 
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
-      Hello
+    <div className="flex flex-col items-center justify-center bg-black h-screen space-y-4  ">
+      <Link href="/signup">
+        <button className="px-4 py-2 bg-white text-black border rounded-xl hover:bg-stone-300">
+          Signup Page
+        </button>
+      </Link>
+      <Link href="/dashboard">
+        <button className="px-4 py-2 bg-white text-black border rounded-xl hover:bg-orange-200">
+          Dashboard Page 
+        </button>
+      </Link>
     </div>
   );
 }
